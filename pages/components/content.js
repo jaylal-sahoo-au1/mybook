efb3920f50dd661116e9b0152dd26233d2af766c
Jaylal <jl@pooraa.com>
2021-09-19 00:33:52 +0530

    mobile view changes

--- pages/components/content.js
+++ pages/components/content.js
@@ -12,8 +12,9 @@ const useStyles = makeStyles((theme) => ({
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center',
-		height: '60vh',
+		height: '100%',
 		flexDirection: 'column',
+		padding: '15px',
 	},
 	textWithMarginBt: {
 		letterSpacing: '5px',
@@ -36,24 +37,16 @@ const Content = () => {
 				<Typography variant="h5" className={classes.text}>
 					START YOUR JOURNEY - WEB DEVELOPMENT
 				</Typography>
-				<Typography
-					variant="p"
-					component="div"
-					className={classes.textWithMargintp}
-				>
+				<Typography component="div" className={classes.textWithMargintp}>
 					We provide a platform where you can learn
 				</Typography>
-				<Typography
-					variant="p"
-					component="div"
-					className={classes.textWithMarginBt}
-				>
+				<Typography component="div" className={classes.textWithMarginBt}>
 					and start your web development career
 				</Typography>
-				<Typography variant="p" component="div" className={classes.text}>
+				<Typography component="div" className={classes.text}>
 					We have separate sections for each course which includes
 				</Typography>
-				<Typography variant="p" component="div" className={classes.text}>
+				<Typography component="div" className={classes.text}>
 					frequently asked interview questions and related project
 				</Typography>
 			</Box>
--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import { useState } from 'react';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { Link, animateScroll as scroll } from 'react-scroll';
 
 const useStyles = makeStyles((theme) => ({
@@ -21,11 +25,26 @@ const useStyles = makeStyles((theme) => ({
 		justifyContent: 'center',
 		flexGrow: 1,
 	},
+	menu: {
+		display: 'flex',
+		justifyContent: 'right',
+		flexGrow: 1,
+	},
 }));
 
 const Navbar = (props) => {
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
 	const classes = useStyles();
+	const isMobile = useMediaQuery('(max-width:600px');
 
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 	return (
 		<div className={classes.navRoot}>
 			<AppBar position="fixed" className={classes.toolbar}>
@@ -37,45 +56,122 @@ const Navbar = (props) => {
 					>
 						My Book
 					</Typography>
-					<Typography className={classes.links}>
-						<Link
-							to=""
-							className="linkBtn"
-							smooth={true}
-							duration={500}
-							spy={true}
-						>
-							Projects
-						</Link>
-						<Link
-							to="https://material-ui.com/components/avatars/#image-avatars"
-							className="linkBtn"
-							smooth={true}
-							duration={500}
-							spy={true}
-						>
-							Technologies
-						</Link>
-						<Link
-							to="https://material-ui.com/components/avatars/#image-avatars"
-							className="linkBtn"
-							smooth={true}
-							duration={500}
-							spy={true}
-						>
-							About
-						</Link>
-						<Link
-							to="courses"
-							smooth={true}
-							duration={100}
-							className="linkBtn"
-							spy={true}
-							activeClass="active"
-						>
-							Courses
-						</Link>
-					</Typography>
+					{isMobile ? (
+						<Typography className={classes.menu}>
+							<MenuIcon
+								id="menu-button"
+								aria-controls="menu-button"
+								aria-haspopup="true"
+								aria-expanded={open ? 'true' : undefined}
+								onClick={handleClick}
+							/>
+							<Menu
+								id="mybook-menu"
+								aria-labelledby="mybook-menu"
+								anchorEl={anchorEl}
+								open={open}
+								onClose={handleClose}
+								anchorOrigin={{
+									vertical: 'top',
+									horizontal: 'left',
+								}}
+								transformOrigin={{
+									vertical: 'top',
+									horizontal: 'left',
+								}}
+							>
+								<MenuItem>
+									<Link
+										to=""
+										className="linkBtn"
+										smooth={true}
+										duration={500}
+										spy={true}
+										onClick={handleClose}
+									>
+										Projects
+									</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link
+										to=""
+										className="linkBtn"
+										smooth={true}
+										duration={500}
+										spy={true}
+										onClick={handleClose}
+									>
+										Technologies
+									</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link
+										to=""
+										className="linkBtn"
+										smooth={true}
+										duration={500}
+										spy={true}
+										onClick={handleClose}
+									>
+										About
+									</Link>
+								</MenuItem>
+								<MenuItem>
+									<Link
+										to="courses"
+										smooth={true}
+										duration={100}
+										className="linkBtn"
+										spy={true}
+										activeClass="active"
+										onClick={handleClose}
+									>
+										Courses
+									</Link>
+								</MenuItem>
+							</Menu>
+						</Typography>
+					) : (
+						<Typography className={classes.links}>
+							<Link
+								to=""
+								className="linkBtn"
+								smooth={true}
+								duration={500}
+								spy={true}
+							>
+								Projects
+							</Link>
+							<Link
+								to="https://material-ui.com/components/avatars/#image-avatars"
+								className="linkBtn"
+								smooth={true}
+								duration={500}
+								spy={true}
+							>
+								Technologies
+							</Link>
+							<Link
+								to="https://material-ui.com/components/avatars/#image-avatars"
+								className="linkBtn"
+								smooth={true}
+								duration={500}
+								spy={true}
+							>
+								About
+							</Link>
+							<Link
+								to="courses"
+								smooth={true}
+								duration={100}
+								className="linkBtn"
+								spy={true}
+								activeClass="active"
+							>
+								Courses
+							</Link>
+						</Typography>
+					)}
 				</Toolbar>
 			</AppBar>
 		</div>
