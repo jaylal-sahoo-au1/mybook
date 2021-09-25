34cc9e9fbe2390437c887293b7543e24d0f4056b
Jaylal <jl@pooraa.com>
2021-09-26 03:52:24 +0530

    live url added-updates

--- pages/htmlCssTemplate.js
+++ pages/htmlCssTemplate.js
@@ -29,9 +29,7 @@ export default function HTMLTemplate(props) {
 }
 
 export async function getServerSideProps(context) {
-	const res = await fetch(
-		'https://mybook-theta.vercel.app/api/htmlcsstemplate'
-	);
+	const res = await fetch('http://mybook-theta.vercel.app/api/htmlcsstemplate');
 	const data = await res.json();
 
 	return {
