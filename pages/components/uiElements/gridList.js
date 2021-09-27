a1fe22891385b3674c5f33580cea89de7540e807
Rahul <pradhanrahul3346@gmail.com>
2021-09-27 16:57:53 +0530

    css added

--- pages/components/uiElements/gridList.js
+++ pages/components/uiElements/gridList.js
@@ -17,6 +17,7 @@ import CodeIcon from '@material-ui/icons/Code';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 const useStyles = makeStyles((theme) => ({
+<<<<<<< Updated upstream
 	expand: {
 		transform: 'rotate(0deg)',
 		marginLeft: 'auto',
@@ -45,28 +46,96 @@ const useStyles = makeStyles((theme) => ({
 	linkedIn: {
 		color: '#0a66c2',
 	},
+=======
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+  htmlRoot: {
+    flexGrow: 1,
+    padding: '16px',
+  },
+  cardcontent1: {
+    height: '50vh',
+    overflow: 'auto',
+  },
+  cardcontent2: {
+    height: '80px',
+    overflow: 'auto',
+  },
+  linkedIn: {
+    color: '#0a66c2',
+  },
+  imageLayout: {
+    height: '100%',
+    width: '100%',
+    position: 'relative',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+  },
+  hoverly: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    opacity: 0,
+    transition: 'opacity 0.25s',
+    background: 'rgba(0,0,0,0.6)',
+
+    '&:hover': {
+      opacity: 1,
+    },
+  },
+  btnLive: {
+    background: '#fff',
+    color: '#000',
+    transition: 'all 0.25s',
+    '&:hover': {
+      background: '#cec6c6c7',
+      color: '#000',
+    },
+  },
+>>>>>>> Stashed changes
 }));
 
 export default function GridList(props) {
-	const classes = useStyles();
-	const [expanded, setExpanded] = React.useState(false);
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+  const myLoader = ({ src, width, quality }) => {
+    return `${src}?w=${width}&q=${quality || 100}`;
+  };
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-	const handleExpandClick = () => {
-		setExpanded(!expanded);
-	};
+  const styleFn = (item) => {
+    if (item?.img) {
+      return classes.cardcontent2;
+    } else {
+      return classes.cardcontent1;
+    }
+  };
 
-	const styleFn = (item) => {
-		if (item?.img) {
-			return classes.cardcontent2;
-		} else {
-			return classes.cardcontent1;
-		}
-	};
+  const redirect = (item) => {
+    window.open(`${item}`, '_blank');
+  };
 
-	const redirect = (item) => {
-		window.open(`${item}`, '_blank');
-	};
-
+<<<<<<< Updated upstream
 	return (
 		<React.Fragment>
 			<Toolbar></Toolbar>
@@ -158,4 +227,116 @@ export default function GridList(props) {
 			</div>
 		</React.Fragment>
 	);
+=======
+  return (
+    <React.Fragment>
+      <Toolbar></Toolbar>
+      <div className={classes.htmlRoot} role="main">
+        <Grid container spacing={2}>
+          {props.data &&
+            props.data.map((list, idx) => (
+              <Grid item xs={12} md={6} lg={4} key={idx}>
+                <Card>
+                  <CardHeader
+                    action={
+                      <Tooltip
+                        title={
+                          list?.actionButton?.linkedIn?.title ||
+                          'Go to LinkedIn profile'
+                        }
+                        onClick={() =>
+                          redirect(list?.actionButton?.linkedIn?.url)
+                        }
+                      >
+                        <IconButton aria-label="settings">
+                          <LinkedInIcon className={classes.linkedIn} />
+                        </IconButton>
+                      </Tooltip>
+                    }
+                    title={list.title}
+                    subheader={list.subheader}
+                  />
+                  {list?.img ? (
+                    <div className={classes.imageLayout}>
+                      <Image
+                        loader={myLoader}
+                        src={list.img}
+                        alt="Browser not supporting"
+                        width={500}
+                        height={333}
+                        className={
+                          list?.actionButton?.livelink?.url ? classes.image : ''
+                        }
+                      />
+                      <div className={classes.hoverly}>
+                        <Button
+                          varaint="contained"
+                          className={classes.btnLive}
+                          onClick={() =>
+                            list?.actionButton?.livelink?.url
+                              ? redirect(list?.actionButton?.livelink?.url)
+                              : ''
+                          }
+                        >
+                          Go Live
+                        </Button>
+                      </div>
+                    </div>
+                  ) : null}
+                  <CardContent className={styleFn(list)}>
+                    {list?.para ? (
+                      <Typography component="div" variant="body2">
+                        {list.para}
+                      </Typography>
+                    ) : null}
+                    {list?.paragraph ? (
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        {list.paragraph}
+                      </Typography>
+                    ) : null}
+                  </CardContent>
+                  <CardActions disableSpacing>
+                    {list?.actionButton?.github ? (
+                      <Tooltip title={list.actionButton.github.title}>
+                        <IconButton
+                          aria-label={list.actionButton.github.title}
+                          onClick={() => redirect(list.actionButton.github.url)}
+                        >
+                          <GitHubIcon />
+                        </IconButton>
+                      </Tooltip>
+                    ) : null}
+                    {list?.actionButton?.blog ? (
+                      <Tooltip title={list.actionButton.blog.title}>
+                        <IconButton
+                          aria-label={list.actionButton.blog.title}
+                          onClick={() => redirect(list.actionButton.blog.url)}
+                        >
+                          <BookIcon />
+                        </IconButton>
+                      </Tooltip>
+                    ) : null}
+                    {list?.actionButton?.code ? (
+                      <Tooltip title={list.actionButton.blog.title}>
+                        <IconButton
+                          aria-label={list.actionButton.blog.title}
+                          onClick={() => redirect(list.actionButton.blog.url)}
+                        >
+                          <CodeIcon />
+                        </IconButton>
+                      </Tooltip>
+                    ) : null}
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
+      </div>
+    </React.Fragment>
+  );
+>>>>>>> Stashed changes
 }
